6d461ac299b41d29367fe8cef9d7fe1b111aff6a
Matthew Maciesowicz <matthewmaciesowicz@gmail.com>
2023-11-08 13:15:36 -0500

    alphanumeric data parsing

--- parser/main.py
+++ parser/main.py
@@ -2,6 +2,15 @@
 
 import pandas as pd
     
-data = pd.read_csv("data/clues.tsv", sep='\t', header=None, on_bad_lines='skip', dtype='unicode')
+df = pd.read_csv("data/clues.tsv", sep='\t', on_bad_lines='skip', dtype='unicode', na_filter=False)
 
-# print(data[0][0])
+#print(df.contains("NULL"))
+# get rid of non-alphanumeric words
+df = df.drop(df[df['answer'].str.contains(r'[^A-Z]',na=False)].index)
+
+# sort data 
+df = df.sort_values('answer', ascending=True)
+print(df)
+
+# print(df.loc[df['answer'] == "NULL"]['clue'])
+# print(df['answer'].max())
