a985c7758a2f706eaa5d89b1fd92de3bb73b079a
Matthew Maciesowicz <matthewmaciesowicz@gmail.com>
2023-11-08 14:33:10 -0500

    save parsed data as json file

--- parser/main.py
+++ parser/main.py
@@ -1,16 +1,21 @@
-# Data courtesy of https://xd.saul.pw/data/
+# Crossword Data from https://xd.saul.pw/data/
 
 import pandas as pd
     
 df = pd.read_csv("data/clues.tsv", sep='\t', on_bad_lines='skip', dtype='unicode', na_filter=False)
 
-#print(df.contains("NULL"))
-# get rid of non-alphanumeric words
+# remove 'pupid' and 'year' columns
+df  = df.drop(columns=['pubid', 'year'], axis=1)
+
+# remove duplicate answers and clues
+df = df.drop_duplicates()
+
+# remove non-alphanumeric words
 df = df.drop(df[df['answer'].str.contains(r'[^A-Z]',na=False)].index)
 
-# sort data 
+# sort answers in alphabetic order
 df = df.sort_values('answer', ascending=True)
-print(df)
 
-# print(df.loc[df['answer'] == "NULL"]['clue'])
-# print(df['answer'].max())
+#print(df.loc[df['answer'] == "AMEN"]['clue'])
+
+df.to_json('data/data.json', orient='records', lines=True)
