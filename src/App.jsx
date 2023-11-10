e3c3f1e1e91e30b83b282eed2c10f5cd9001692c
Matthew Maciesowicz <matthewmaciesowicz@gmail.com>
2023-11-10 15:23:58 -0500

    used UseState hook on variables to keep answers

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Header from './Header.jsx'
 import Words from '../data/data.json'
+import { random } from 'lodash';
 //import useFetch from './useFetch';
 
 function App() {
+
   const [winCount, setWinCount] = useState(0);
   const [loseCount, setLoseCount] = useState(0);
-  
+    
   const getRandomWord = (wordLength) => {
     const randomWord = Words[wordLength][Math.floor(Math.random()*Words[wordLength].length)];
     return [randomWord.word,randomWord.clues];
@@ -15,28 +17,48 @@ function App() {
 
   // generate words from length 3 to 15 inclusive
   const maxLength = 13;
-  let wordLength = (Math.ceil(Math.random()*maxLength))+2;
+  const getMaxLength = (maxLength) => {return (Math.ceil(Math.random()*maxLength))+2};
+  const [wordLength, getWordLength] = useState(getMaxLength(maxLength));
 
-  const getWord = getRandomWord(wordLength);
-  const currentWord = getWord[0];
-  const currentClues = getWord[1];
+  // consists of a 4-element array of an array. At index getWordIndex[i] (0<=i<=3), 
+  // getWordIndex[i][0] is the word, getWordIndex[i][1] is an array of clues for that word 
+  const getRandomWords = (wordLength) => {
+    return [getRandomWord(wordLength),getRandomWord(wordLength),getRandomWord(wordLength),getRandomWord(wordLength)]
+  };
+  const [wordsAndClues, getNewWords] = useState(getRandomWords(wordLength));
 
   // generate random placement for answer in position
-  const winPosition = Math.ceil(Math.random()*4);
-  
-  let buttonDiv = (win = 0, winWord = "") => {
+  const getWinPosition = () => {return Math.floor(Math.random()*4)};
+  const [winPosition, getNewWinPosition] = useState(getWinPosition);
+
+  // generate random placement for answer in position
+  const setupNextWord = () => {
+    setTimeout(() => {
+      getWordLength((wordLength) => wordLength = getMaxLength(maxLength));
+      getNewWords((wordsAndClues) => wordsAndClues = getRandomWords(wordLength));
+      getNewWinPosition((winPosition) => winPosition = getWinPosition());
+    }, 1500);
+  };
+
+  let buttonDiv = (win = 0, index) => {
     if (win) {
       return (
-      
-          <button className='correctAns' onClick={() => setWinCount((winCount) => winCount + 1)}>
-              {winWord}
+          <button className='correctAns' 
+            onClick={() => {
+                  setWinCount((winCount) => winCount + 1);
+                  setupNextWord();
+          }}>
+          {wordsAndClues[index][0]} 
           </button>
       )
     }
     else {
       return (
-          <button onClick={() => setLoseCount((loseCount) => loseCount + 1)}>
-              {getRandomWord(wordLength)[0]}
+          <button onClick={() => {
+            setLoseCount((loseCount) => loseCount + 1)
+            setupNextWord();
+          }}>
+          {wordsAndClues[index][0]}
           </button>
       )
     }
@@ -52,20 +74,20 @@ function App() {
         <div id='lose-label' className='badge'>lose</div>
         <span id="win-score">{winCount}</span>:<span id='lose-score'>{loseCount}</span>
       </div>
-      <h1>{currentClues[0]}</h1>
+      <h1>{wordsAndClues[winPosition][0]}</h1>
       
       <div className="buttons-container">
         <div className="btn">
-          {winPosition == 0 ? buttonDiv(1, currentWord) : buttonDiv(0, "")}
+          {winPosition == 0 ? buttonDiv(1, 0) : buttonDiv(0, 0)}
         </div>
         <div className="btn">
-          {winPosition == 1 ? buttonDiv(1, currentWord) : buttonDiv(0, "")}
+          {winPosition == 1 ? buttonDiv(1, 1) : buttonDiv(0, 1)}
         </div>
         <div className="btn">
-          {winPosition == 2 ? buttonDiv(1, currentWord) : buttonDiv(0, "")}
+          {winPosition == 2 ? buttonDiv(1, 2) : buttonDiv(0, 2)}
         </div>
         <div className="btn">
-          {winPosition == 3 ? buttonDiv(1, currentWord) : buttonDiv(0, "")}
+          {winPosition == 3 ? buttonDiv(1, 3) : buttonDiv(0, 3)}
         </div>
         
       </div>
